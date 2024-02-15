d023e6ed61758f284e75b5df3e92df4d97efc5fe
itsRend <104157251+Seinzzz@users.noreply.github.com>
2024-02-15 16:57:42 +0000

    feat: Fix formatting, add tasks.json file in .codesandbox folder, add devcontainer.json file in .devcontainer folder and add dependabot.yml file in .github folder

--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,9 @@
-import { EmbedBuilder } from 'discord.js'
-import type WatcherData from './showroom/watcher/core'
-import type { ShowroomWatcherOptions } from './types/options'
+import { EmbedBuilder } from "discord.js";
+import type WatcherData from "./showroom/watcher/core";
+import type { ShowroomWatcherOptions } from "./types/options";
 
-const liveColor = '#2ddf58'
-const finishColor = '#00b0f4'
+const liveColor = "#2ddf58";
+const finishColor = "#00b0f4";
 
 const config: ShowroomWatcherOptions = {
   delay: {
@@ -16,36 +16,46 @@ const config: ShowroomWatcherOptions = {
     no_activity_refresh: 60000,
   },
   discord: {
-    channel: 'showroom',
-    dev_channel: 'showroom-dev', // if empty channel_name will be used
+    channel: "wots-project-dev",
+    dev_channel: "showroom-dev", // if empty channel_name will be used
     specific_guilds: [],
   },
   message: {
     live: (watcher: WatcherData): EmbedBuilder => {
-      const giftData = watcher.giftLog.getTop()
-      return new EmbedBuilder()
-        .setTitle(watcher.name)
-        .setDescription(`Total Gift : \`${giftData?.total ?? 0}\`\n\`\`\`${giftData?.users || 'No gift right now'}\`\`\``)
-        .setImage(watcher.image)
-        .setColor(liveColor)
-        .setFooter({ text: 'Showroom Gift Counter', iconURL: 'https://play-lh.googleusercontent.com/AKKnVMfKzyuPK2QLY3WDhhfBKyhW8nq5KjYARLcDgcub4ue5B9RSBzjv4-nU5EA1debu' })
-        .setURL(`https://www.showroom-live.com/${watcher.roomKey}`)
-    },
-    live_end: (watcher: WatcherData): EmbedBuilder => {
-      const giftData = watcher.giftLog.getTop()
+      const giftData = watcher.giftLog.getTop();
       return new EmbedBuilder()
         .setTitle(watcher.name)
         .setDescription(
-          `Live ${watcher.name} is finished!` + `\nTotal Gifts : \`${giftData?.total ?? 0}\``,
+          `Total Gift : \`${giftData?.total ?? 0}\`\n\`\`\`${giftData?.users || "No gift right now"}\`\`\``,
+        )
+        .setImage(watcher.image)
+        .setColor(liveColor)
+        .setFooter({
+          text: "Showroom Gift Counter",
+          iconURL:
+            "https://play-lh.googleusercontent.com/AKKnVMfKzyuPK2QLY3WDhhfBKyhW8nq5KjYARLcDgcub4ue5B9RSBzjv4-nU5EA1debu",
+        })
+        .setURL(`https://www.showroom-live.com/${watcher.roomKey}`);
+    },
+    live_end: (watcher: WatcherData): EmbedBuilder => {
+      const giftData = watcher.giftLog.getTop();
+      return new EmbedBuilder()
+        .setTitle(watcher.name)
+        .setDescription(
+          `Live ${watcher.name} is finished!` +
+            `\nTotal Gifts : \`${giftData?.total ?? 0}\``,
         )
         .setImage(watcher.image)
         .setColor(finishColor)
         .setTimestamp(new Date().getTime())
-        .setFooter({ text: 'Showroom', iconURL: 'https://play-lh.googleusercontent.com/AKKnVMfKzyuPK2QLY3WDhhfBKyhW8nq5KjYARLcDgcub4ue5B9RSBzjv4-nU5EA1debu' })
-        .setURL(`https://www.showroom-live.com/${watcher.roomKey}`)
+        .setFooter({
+          text: "Showroom",
+          iconURL:
+            "https://play-lh.googleusercontent.com/AKKnVMfKzyuPK2QLY3WDhhfBKyhW8nq5KjYARLcDgcub4ue5B9RSBzjv4-nU5EA1debu",
+        })
+        .setURL(`https://www.showroom-live.com/${watcher.roomKey}`);
     },
   },
+};
 
-}
-
-export default config
+export default config;
